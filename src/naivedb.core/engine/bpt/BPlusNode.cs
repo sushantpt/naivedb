4bd776cd2da2c28d3fc576b86d91b1f8bb92db71
sushantpt <gu1usends@gmail.com>
2025-11-08 18:24:43 +0545

    refactor: replace PagedFileStorage with PagedFileStorageUsingBPT for improved data handling and indexing feat: add tombstone functionality and dirty node tracking in B+ tree implementation fix: update GetAsync method signatures to return nullable types.

--- src/naivedb.core/engine/bpt/BPlusNode.cs
+++ src/naivedb.core/engine/bpt/BPlusNode.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using MessagePack;
 
 namespace naivedb.core.engine.bpt
 {
@@ -13,20 +14,19 @@ namespace naivedb.core.engine.bpt
         public List<TKey> Keys { get; set; } = [];
         public List<TValue> Values { get; set; } = []; // only for leaf nodes i.e. to store actual data or rowpointers or page numbers
         public List<string> ChildIds { get; set; } = []; // only for internal nodes i.e. to act like a pointer to child nodes
+        public bool NodeIsDirty { get; set; } = false; // dirty nodes to be flushed/reshuffled in the background asynchronously 
+        public List<bool> IsDeleted { get; set; } = []; // tombstone for leafnodes
         
         /*
-         * connect to other pages, sorta doubly linked list 
+         * connect to other pages, sorta linked list 
          */
-        public int? PrevPageNumber { get; set; } = null;
-        public int? NextPageNumber { get; set; } = null;
+        public string? NextNodeId { get; set; }
         
         /*
          * ignore because we dont need to serialize this; reason is that we dont want to serialize the entire tree (so its bit faster)
          */
-        [JsonIgnore]
+        [JsonIgnore] [IgnoreMember]
         public BPlusNode<TKey, TValue>? Parent { get; set; } = null;
-        [JsonIgnore]
-        public List<BPlusTree<TKey, TValue>>? Children { get; set; } = [];
         
         public BPlusNode(bool isLeaf)
         {
@@ -37,5 +37,22 @@ namespace naivedb.core.engine.bpt
         
         public bool IsFull(int order) => Keys.Count >= order; // check if node is full; if so, split
         public bool IsUnderflow(int order) => Keys.Count <= Math.Max(1, (order + 1) / 2); // check if node is underflow; if so, merge
+        
+        public void Tombstone(int index)
+        {
+            if (!IsLeaf) 
+                throw new InvalidOperationException("Only leaf nodes can delete keys.");
+            IsDeleted[index] = true;
+            NodeIsDirty = true;
+        }
+        
+        public IEnumerable<(TKey Key, TValue Value)> GetActiveKeys()
+        {
+            for (int i = 0; i < Keys.Count; i++)
+            {
+                if (!IsDeleted[i])
+                    yield return (Keys[i], Values[i]);
+            }
+        }
     }
 }
--- src/naivedb.core/indexing/IIndex.cs
+++ src/naivedb.core/indexing/IIndex.cs
@@ -9,7 +9,7 @@ namespace naivedb.core.indexing
         Type KeyType { get; }
         Task LoadAsync();
         Task SaveAsync();
-        Task<object>? GetAsync(object key);
+        Task<object?> GetAsync(object key);
         Task InsertAsync(object key, RowPointer ptr);
         Task DeleteAsync(object key);
         Task RebuildAsync(Func<IAsyncEnumerable<(object key, RowPointer ptr)>> scanPredicate);
